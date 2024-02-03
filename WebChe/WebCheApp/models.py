4ac4a58664668c455511b096bafe185fe91c2e3a
ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
2024-02-03 01:03:26 -0500

    imagen principal y clase documentos annadida

--- WebChe/WebCheApp/models.py
+++ WebChe/WebCheApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Repository(models.Model):
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    name=models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Documento"
+        verbose_name_plural = "Documentos"
+        # ordering = ['name']
