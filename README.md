17809840f9ee1b40cc5fc9cdb930e8f00d7cda22
ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
2024-02-02 23:35:25 -0500

    base del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Proyecto-Web-Che
+
+Pagina web hecha en Django para la difusion de informacion sobre la vida del Che Guevara.
---
+++ WebChe/WebCheApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebcheappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'WebCheApp'
